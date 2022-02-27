d87a3a3846b396b232289682757d45a3bcbaa0e2
和泉貴徳 <kanousei16@gmail.com>
2022-02-27 18:39:09 +0900

    initial commit

---
+++ src/router/Page2Routes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Page2 } from '../Page2'
+import { UrlParameter } from '../UrlParameter'
+
+export const Page2Routes = [
+    {
+        path: "/",
+        exact: true,
+        children: <Page2 />
+    },
+    {
+        path: "/:id",
+        exact: false,
+        children: <UrlParameter />
+    }
+
+]
